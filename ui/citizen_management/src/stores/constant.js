b83adf215bd289bce939da3268eea2e1d61a4360
ngxson4160 <ng.xson194160@gmail.com>
2024-05-27 20:53:41 +0700

    ad constant fe

---
+++ ui/citizen_management/src/stores/constant.js
@@ -0,0 +1,13 @@
+export const initData = {
+  roleUser: ['Tổ trưởng', 'Tổ phó', 'Kế toán', 'Người dân'],
+  genderPerson: ['Nam', 'Nữ', 'Khác'],
+  relationPerson: ['Chủ hộ', 'Chồng/Vợ', 'Con', 'Bố', 'Mẹ', 'Khác'],
+  cycleType: ['Theo ngày', 'Theo tháng', 'Theo năm'],
+  notePerson: ['Không', 'Mới sinh', 'Đăng ký tạm trú, tạm vắng', 'Đã mất'],
+};
+
+// const modifyHouseholdRole = [0, 1];
+
+// const modifyPersonRole = [0, 1];
+
+// const modifyMoneyRole = [0, 1, 2];
