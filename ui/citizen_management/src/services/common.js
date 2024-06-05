c801b0b848f3e8a90bc1b62fb24daa5a04c2c5aa
SSylvie19 <hienhathe1911@gmail.com>
2024-06-05 23:52:01 +0700

    PeopleServices

--- ui/citizen_management/src/services/common.js
+++ ui/citizen_management/src/services/common.js
@@ -3,7 +3,18 @@ const API_ROOT = process.env.REACT_APP_API_ROOT || '';
 const APIS = {
   user: {},
   household: {},
-  people: {},
+  people: {
+    getPeople: () => axios.get(`${API_ROOT}/person`),
+    getPersonDetail: (personId) => axios.get(`${API_ROOT}/person/${personId}`),
+    addPerson: (data) => axios.post(`${API_ROOT}/person`, data),
+    editPerson: (personId, data) =>
+      axios.put(`${API_ROOT}/person/${personId}`, data),
+    deletePerson: (personId) => axios.delete(`${API_ROOT}/person/${personId}`),
+    registerForTemporaryResidence: (personId, data) =>
+      axios.post(`${API_ROOT}/person/${personId}/residence`, data),
+    reportDeath: (personId, data) =>
+      axios.post(`${API_ROOT}/person/${personId}/death-report`, data),
+  },
   money: {},
 };
 
