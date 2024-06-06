2c74ae9a79e17fe54e1145ee0733cc68c4a6cf55
Do Tu <dotu2832001@gmail.com>
2024-06-06 20:36:02 +0700

    services - money

--- ui/citizen_management/src/services/common.js
+++ ui/citizen_management/src/services/common.js
@@ -4,7 +4,18 @@ const APIS = {
   user: {},
   household: {},
   people: {},
-  money: {},
+  money: {
+    getMoney: () => axios.get(`${API_ROOT}/money`),
+    getPeriodMoney: () => axios.get(`${API_ROOT}/money/period`),
+    getContributionMoney: () => axios.get(`${API_ROOT}/money/contribution`),
+    addMoney: (data) => axios.post(`${API_ROOT}/money`, data),
+    editMoney: (moneyId, data) =>
+      axios.put(`${API_ROOT}/money/${moneyId}`, data),
+    deleteMoney: (moneyId) => axios.delete(`${API_ROOT}/money/${moneyId}`),
+    getMoneyDetail: (moneyId) => axios.get(`${API_ROOT}/money/${moneyId}`),
+    updateMoneyCollectStatus: (moneyId, data) =>
+      axios.put(`${API_ROOT}/money/${moneyId}/collect-status`, data),
+  },
 };
 
 export default APIS;
